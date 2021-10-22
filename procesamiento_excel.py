7f34355cf32f90b7286865353b30fe6408e70f69
jparada <jparada@edutecno.com>
2021-10-22 16:35:32 -0300

    Filtro sollicitado

--- procesamiento_excel.py
+++ procesamiento_excel.py
@@ -25,11 +25,24 @@ df.head()
 df.tail()
 df.sample(6)
 #Filtramos la seccion de copiadoras
-print("Filtramos")
-Sección=df[df.Sección == "Copiadoras"]
-Sección
+Fax=df[df.Sección=="Fax"]
+Fax
+print (Fax)
+
+Copiadoras=df[df.Sección=="Copiadoras"]
+Copiadoras
+df=Copiadoras
+print (Copiadoras)
+
+Impresoras=df[df.Sección=="Impresoras"]
+Impresoras
+print (Impresoras)
+
+SumaSalario=df.Salario.sum()
+SumaSalario
+print (SumaSalario)
 #Generamos un excel con el procesado resultante y se guarda con el nombre generacion_excel
 writer = ExcelWriter('generacion_excel.xlsx')
-df.to_excel(writer, 'Hoja de datos', index=False)
+df.to_excel(writer, 'ETL', index=False)
 writer.save()
 print("Ejemplo de rama y modificacion de archivo")
